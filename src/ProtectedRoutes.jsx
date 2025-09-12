535a8f9fe5624d588613ed6c3d62328a7b64d8eb
kshakya154 <kshakya154@gmail.com>
2025-09-12 11:52:53 +0530

    Add loading component for ProtectedRoute and update loading state handling

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -1,10 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import React from "react";
+import Example from "./components/ui/Loader";
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
-  if (loading) return <p>Loading...</p>; // wait while checking token
+  if (loading) return <Example/>; // wait while checking token
   if (!user) return <Navigate to="/login" replace />; // if no user → redirect
 
   return children; // authorized → show page
